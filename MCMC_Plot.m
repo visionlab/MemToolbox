8eeed97bc82dab8737cfedd0632a0913b2f210f9
Tim Brady <timothy.brady@gmail.com>
2012-03-11 20:06:30 -0400

    use imagesc() instead of mesh()

--- MCMC_Plot.m
+++ MCMC_Plot.m
@@ -1,24 +1,25 @@
-function MCMC_Plot(stored, paramNames)
+function figHand = MCMC_Plot(stored, paramNames)
     % Show 2x2 correlation for each variable with each other to look for
     % structure; Visualize both as a scatter and as a 2D histogram
-    figure;
+    figHand = figure;
     N = length(paramNames);
     for p=1:N
         for p2=1:(p-1)
             subplot(N,N,sub2ind([N N],p,p2));
-            hist3(stored.vals(:,[p p2]), [20 20]);
-            set(get(gca,'child'), 'FaceColor', 'interp', 'CDataMode', ...
-                'auto', 'LineWidth', 0.001, 'EdgeColor', 'none');
-            view(2); axis tight;
+            [V,C] = hist3(stored.vals(:,[p p2]), [20 20]);
+            imagesc(C{1}, C{2}, V);
+            set(gca,'YDir','normal');
+            axis tight;
             
             subplot(N,N,sub2ind([N N],p2,p));
-            hist3(stored.vals(:,[p p2]), [20 20]);
-            set(get(gca,'child'), 'FaceColor', 'interp', 'CDataMode', ...
-                'auto', 'LineWidth', 0.001, 'EdgeColor', 'none');
-            view(2); axis tight;
+            [V,C] = hist3(stored.vals(:,[p p2]), [20 20]);
+            imagesc(C{2}, C{1}, V');
+            set(gca,'YDir','normal');
+            axis tight;
         end
         subplot(N,N,sub2ind([N N],p,p));
         hist(stored.vals(:,p));
+        set(gca, 'YTick', []);
         subplot(N,N,sub2ind([N N],p,1));
         title(paramNames{p}, 'FontSize', 15);
         subplot(N,N,sub2ind([N N],1,p));
