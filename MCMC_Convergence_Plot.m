db37ac80b0df71574b32bee5e6c2c30b4cc57d8b
Tim Brady <timothy.brady@gmail.com>
2012-03-11 20:04:46 -0400

    Fix colors and spacing on Convergence Plot

--- MCMC_Convergence_Plot.m
+++ MCMC_Convergence_Plot.m
@@ -1,10 +1,11 @@
-function MCMC_Convergence_Plot(stored, paramNames)
+function figHand = MCMC_Convergence_Plot(stored, paramNames)
   % Show traces for each variable for each MCMC chain. Very simple way to
   % diagnose convergence: They should look the same as each other.
-  figure;
+  figHand = figure;
   N = length(paramNames);
-  colors = {'r', 'g', 'b', 'y', 'm', 'c'};
+  colors = {'b', 'g', 'r', 'c', 'm', 'y'};
   for p=1:N
+    % Make trace plots
     h = subplot(N,3,sub2ind([3 N],1:2,[p p]));
     for c=1:max(stored.chain)
       vals(:,c) = stored.vals(stored.chain==c, p);
@@ -14,6 +15,7 @@ function MCMC_Convergence_Plot(stored, paramNames)
     set(gca, 'XTick', []);
     title(paramNames{p}, 'FontSize', 15);
     
+    % Make overlapping histograms
     h2 = subplot(N,3,sub2ind([3 N],3,p));
     yBins = linspace(lims(3), lims(4), 30)';
     for c=1:max(stored.chain)
@@ -23,21 +25,15 @@ function MCMC_Convergence_Plot(stored, paramNames)
         colors{mod(c-1, length(colors))+1}); %'FaceAlpha', .2 
       hold on;
     end    
+    % Match axes of trace plots
     ylim([lims(3), lims(4)]);
     set(gca, 'YTick', []);
     set(gca, 'XTick', []);
     
+    % Make trace plots a bit wider
     pos = get(h, 'Position');
     pos(3) = pos(3) + 0.05;
-    pos(2) = pos(2) + 0.04*(p-1);
-    set(h, 'Position', pos);
-    pos = get(h2, 'Position');
-    pos(2) = pos(2) + 0.04*(p-1);
-    set(h2, 'Position', pos);    
-    
-    txt = axes('Position',[0.1 0 0.8 1],'Visible','off');
-    str(1) = {'All of the colored traces and histograms should overlap.'};
-    str(2) = {'This shows all of the chains converged to the same place.'};
-    text(0.04,.07, str, 'FontSize',10);
+    set(h, 'Position', pos); 
   end
+  set(gcf,'Color',[1 1 1]);
 end
