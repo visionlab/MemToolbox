05bf0deb079a5a0bcf584b4098dda6aa430eb38e
Tim Brady <timothy.brady@gmail.com>
2012-03-11 20:05:29 -0400

    Make .pdf work with any x data, but cache for speed.

--- MemModels/InfiniteScaleMixtureModel.m
+++ MemModels/InfiniteScaleMixtureModel.m
@@ -7,32 +7,42 @@
 
 % TODO: Convert to use Memoize() instead of this way of caching
 
-function model = InfiniteScaleMixtureModel(data)
+function model = InfiniteScaleMixtureModel()
 	model.paramNames = {'g', 'sigma', 'df'};
 	model.lowerbound = [0 0 0]; % Lower bounds for the parameters
 	model.upperbound = [1 Inf Inf]; % Upper bounds for the parameters
 	model.movestd = [0.02, 0.1, 0.25];
-	model.fastWrappedT = CreateFastWrappedT(data);
-	model.pdf = @(data, g, sigma, df) (1-g).*model.fastWrappedT(data,0,sigma,df) + (g).*unifpdf(data,-pi,pi)';
+	model.pdf = @(data, g, sigma, df) ...
+    (1-g).*tDistWrapped(data,0,sigma,df)' + (g).*unifpdf(data,-pi,pi);
 	model.start = [0.0, 0.2, 0.2;
                    0.2, 0.3, 1.0;
                    0.4, 0.1, 2.0;
                    0.6, 0.5, 5.0];
 end
 
-
-function y = tlspdf(x, mu, sigma, df)
-     y = tpdf((x - mu)./sigma,df)./sigma;
+function p = tDistWrapped(x, mu, sigma, df)
+  persistent m;
+  if isempty(m)
+    m = containers.Map();
+  end
+  hash = DataHash(x);
+  if ~m.isKey(hash)
+    m(hash) = CreateFastWrappedT(x);
+  end
+  f = m(hash);
+  p = f(mu,sigma,df);
 end
 
-
 function f = CreateFastWrappedT(x)
-    valsRange = 100;
-    addVals = (-valsRange:valsRange)' * (zeros(1,length(x(:)'))+2*pi);
-    xVals = repmat(x(:)', [valsRange*2+1, 1]);
-    combinedX = addVals+xVals;
-    f = @(x,mu,sigma,df)(sum( ...
-        (exp(gammaln((df + 1) / 2) - gammaln(df/2)) ...
-        ./ (sqrt(df*pi).*(1+(((combinedX-mu)./sigma).^2)./df).^((df + 1)/2))) ...
-        ./sigma, 1));
+  valsRange = 100;
+  addVals = (-valsRange:valsRange)' * (zeros(1,length(x))+2*pi);
+  if size(x,1) > size(x,2)
+    x = x';
+  end
+  xVals = repmat(x, [valsRange*2+1, 1]);
+  combinedX = addVals+xVals;
+  f = @(mu,sigma,df)(sum( ...
+    (exp(gammaln((df + 1) / 2) - gammaln(df/2)) ...
+    ./ (sqrt(df*pi).*(1+(((combinedX-mu)./sigma).^2)./df).^((df + 1)/2))) ...
+    ./sigma, 1));
 end
