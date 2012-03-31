08da345a957a4e33ba12ca8498e410f1af26db08
Jordan Suchow <suchow@fas.harvard.edu>
2012-03-31 03:02:33 -0400

    added binomial/vonmises option

--- MemModels/InfiniteScaleMixtureModel.m
+++ MemModels/InfiniteScaleMixtureModel.m
@@ -12,6 +12,9 @@ function model = InfiniteScaleMixtureModel(baseDistribution, mixingDistribution)
     if(strcmp(baseDistribution, 'wrappednormal') && strcmp(mixingDistribution, 'gamma'))
         model = StudentsTModel();
         return;
+    elseif(strcmp(baseDistribution, 'vonmises') && strcmp(mixingDistribution, 'binomial')
+        model = BinomialModel();
+        return;
     else
         error('The requested combination of base and mixing distributions is not supported.')
     end
