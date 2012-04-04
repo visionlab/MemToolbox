27de4d9f529ca4573fddb5e07d5991472d138c53
Jordan Suchow <suchow@fas.harvard.edu>
2012-04-04 15:03:28 -0400

    it works now, though the parameters trade off so strongly that the start and movestd needs to be tuned a bit.

--- MemModels/BinomialModel.m
+++ MemModels/BinomialModel.m
@@ -1,13 +1,10 @@
 function model = BinomialModel()
   model.paramNames = {'g', 'nQ', 'p', 's'};
   model.lowerbound = [0 1 0 0]; % Lower bounds for the parameters
-  model.upperbound = [1 Inf 1 Inf]; % Upper bounds for the parameters
-  model.movestd = [0.02, 1, 0.02, 1];
+  model.upperbound = [1 100 1 Inf]; % Upper bounds for the parameters
+  model.movestd = [0.1, 2, 0.05, 2];
   model.pdf = @binomialpdf;
-  model.start = [0.0, 10, 0.2, 10;
-                   0.2, 100, 0.3, 1;
-                   0.4, 1, 0.1, 2;
-                   0.6, 40, 0.9, 50];
+  model.start = [0.0, 10, 0.2, 11];
 end
   
 function y = binomialpdf(data,g,nQ,p,s)
@@ -16,14 +13,15 @@ function y = binomialpdf(data,g,nQ,p,s)
     q = s*sqrt(nQ); % the precision of 1 quantum
     
     % precompute uniform component
-    uni = unifpdf(data,-pi,pi);
+    yUniform = unifpdf(data,-pi,pi);
     
-    % precompute binomial component
+    % precompute binomial probabilities
     bino = binopdf([0:nQ], nQ, p);
-        
-    y = 0;
+    
+    % compute binomial component
+    yBinomial = 0;
     for i = 0:nQ
-       y = y + (1-g).*bino(i+1).*vonmisespdf(data,0,sd2k(q./sqrt(i))) + ...
-                 (g).*uni;
+       yBinomial = yBinomial + bino(i+1).*vonmisespdf(data,0,sd2k(q./sqrt(i)));
     end 
+    y = (1-g).*yBinomial + (g).*yUniform;
 end
