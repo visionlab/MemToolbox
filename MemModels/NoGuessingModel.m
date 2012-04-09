205f71af2ba943c6ce835063a0f11196ca1198db
Jordan Suchow <suchow@fas.harvard.edu>
2012-04-09 11:48:04 -0400

    fixed documentation

--- MemModels/NoGuessingModel.m
+++ MemModels/NoGuessingModel.m
@@ -7,7 +7,7 @@ function model = NoGuessingModel()
 	model.upperbound = [Inf]; % Upper bounds for the parameters
 	model.movestd = [0.1];
 	model.pdf = @(data, K) (vonmisespdf(data,0,K));
-	model.start = [10;  % g, K
-                   15;  % g, K
-                   20]; % g, K
+	model.start = [10;  % K
+                   15;  % K
+                   20]; % K
 end
