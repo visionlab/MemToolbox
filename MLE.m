c22169aca28ed0cacebaa2a5ae10c19b5605eb95
Tim Brady <timothy.brady@gmail.com>
2012-04-09 18:39:28 -0400

    minor fixes

--- MLE.m
+++ MLE.m
@@ -5,14 +5,12 @@
 function [params, stored] = MLE(data, model)
   % Fastest if your number of start positions is the same as the number
   % of cores/processors you have
-  if matlabpool('size') == 0
-    matlabpool('open');
-  end
-  
+  options = statset('MaxIter',2500, 'MaxFunEvals',2500,'UseParallel', 'always');
   numChains = size(model.start,1);
-  for c=1:numChains
+  parfor c=1:numChains
     vals{c} = mle(data, 'pdf', model.pdf, 'start', model.start(c,:), ...
-        'lowerbound', model.lowerbound, 'upperbound', model.upperbound);
+        'lowerbound', model.lowerbound, 'upperbound', model.upperbound, ...
+        'options', options);
     asCell = num2cell(vals{c});
     like(c) = sum(log(model.pdf(data, asCell{:})));
   end
