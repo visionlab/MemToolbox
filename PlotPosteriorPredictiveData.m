cf5d03a6226837072c241b76195afe742bc271ea
Tim Brady <timothy.brady@gmail.com>
2012-04-13 14:39:10 -0400

    clean up colors to match other plots (model = colored; data = gray)

--- PlotPosteriorPredictiveData.m
+++ PlotPosteriorPredictiveData.m
@@ -1,11 +1,14 @@
-function figHand = PlotPosteriorPredictiveData(model, stored, data)
+function figHand = PlotPosteriorPredictiveData(model, stored, data, modelColor)
   % Show data sampled from the model with the actual data overlayed, plus a
   % difference plot.
+  if nargin < 4
+    modelColor = 'b';
+  end
   
   figHand = figure();
   
   % Choose which samples to use
-  numSamplesToPlot = 128;
+  numSamplesToPlot = 48;
   which = round(linspace(1, size(stored.vals,1), numSamplesToPlot));
   
   % How to bin
@@ -24,15 +27,18 @@ function figHand = PlotPosteriorPredictiveData(model, stored, data)
     % Bin data and model
     n = hist(yrep, x)';
     n = n ./ sum(n(:));
-    plot(x, n, '-', 'Color', [.5 .5 .5]);
+    plot(x, n, '-', 'Color', modelColor);
     
     % Diff between this data and real data
     diffPlot(i,:) = nData - n;
   end  
 
   % Plot data
-  plot(x,nData,'r-','LineWidth',2);
+  h=plot(x,nData,'ok-','LineWidth',2, ...
+    'MarkerEdgeColor',[.1 .1 .1],'MarkerFaceColor', [.5 .5 .5], ...
+    'MarkerSize', 5);
   title('Simulated data from model');
+  legend(h, 'Actual data');
   xlim([-pi, pi]);
   
   % Plot difference
@@ -43,6 +49,7 @@ function figHand = PlotPosteriorPredictiveData(model, stored, data)
   line([-pi pi], [0 0], 'LineStyle', '--', 'Color', [.5 .5 .5]);
   xlim([-pi pi]);
   title('Difference between real data and simulated data');
+  xlabel('(deviations from zero indicate bad fit)');
   palettablehistogram();
 end
 
