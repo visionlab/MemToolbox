8b1579f04b6e09891725a8eb0a1986f187a822d5
Tim Brady <timothy.brady@gmail.com>
2012-04-23 11:10:42 -0400

    use logpdf

--- MLE.m
+++ MLE.m
@@ -5,14 +5,22 @@
 function [params, stored] = MLE(data, model)
   % Fastest if your number of start positions is the same as the number
   % of cores/processors you have
-  options = statset('MaxIter',2500, 'MaxFunEvals',2500,'UseParallel', 'always');
+  options = statset('MaxIter',5000, 'MaxFunEvals',5000,'UseParallel','always');
   numChains = size(model.start,1);
-  parfor c=1:numChains
-    vals{c} = mle(data, 'pdf', model.pdf, 'start', model.start(c,:), ...
+  for c=1:numChains
+    if isfield(model, 'logpdf')
+      vals{c} = mle(data, 'logpdf', model.logpdf, 'start', model.start(c,:), ...
         'lowerbound', model.lowerbound, 'upperbound', model.upperbound, ...
         'options', options);
-    asCell = num2cell(vals{c});
-    like(c) = sum(log(model.pdf(data, asCell{:})));
+      asCell = num2cell(vals{c});
+      like(c) = model.logpdf(data, asCell{:});
+    else
+      vals{c} = mle(data, 'pdf', model.pdf, 'start', model.start(c,:), ...
+        'lowerbound', model.lowerbound, 'upperbound', model.upperbound, ...
+        'options', options);
+      asCell = num2cell(vals{c});
+      like(c) = sum(log(model.pdf(data, asCell{:})));
+    end
   end
   
   % Combine values across chains
