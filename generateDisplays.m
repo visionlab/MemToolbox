c2397e7c461f5a12db8f485757a2dfb6215edd8b
Jordan Suchow <suchow@fas.harvard.edu>
2012-04-24 03:09:42 -0400

    minor changes

--- generateDisplays.m
+++ generateDisplays.m
@@ -1,13 +1,16 @@
 function displays = generateDisplays(trials, itemsPerTrial, mode)
     
-    % default mode is 0 (iid from circular)	
+  % default mode is 0 (iid from circular)	
 	if nargin < 3
-	   mode = 0; 
-    end
+	  mode = 1; 
+  end
 	
 	% iid from ciruclar uniform
-	if mode == 0
-	    displays = unifrnd(-pi, pi, trials, itemsPerTrial);
-	end
+	if mode == 1
+	  displays = unifrnd(-pi, pi, trials, itemsPerTrial);
+	else
+	  warning('No such mode, defaulting to iid vm.')
+	  displays = generateDisplays(trials,itemsPerTrial,0);
+  end
 
 end
