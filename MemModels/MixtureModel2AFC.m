f5bad3efefe01610b94b45dcaebcd92e6af65c61
Tim Brady <timothy.brady@gmail.com>
2012-04-24 21:04:09 -0400

    i always find myself wishing we had a mixture model parameterized with SD. wasn't sure whether to make it an option when you create StandardMixtureModel or make it a new model...

--- MemModels/MixtureModel2AFC.m
+++ MemModels/MixtureModel2AFC.m
@@ -1,11 +1,6 @@
 % MixtureModel2AFC returns a structure for a two-component mixture model
 % that can be fit to 2afc data
 
-% Data format should be:
-%   Row 1: binary data -- 1 for correct, 0 for incorrect
-%   Row 2: the size, in radians, of the change that observers were shown
-% (the difference between color 1 and color 2)
-
 function model = MixtureModel2AFC(minGuessRate)
   % Allow minimum guess rate if we know that subjects will always be
   % guessing more than a certain proportion of the time
@@ -22,6 +17,11 @@ function model = MixtureModel2AFC(minGuessRate)
   model.pdf = @Mixture2AFCpdf;
 end
 
+% pdf:
+% Expects format 2xN, where N is number of datapoints
+% Row 1 is the binary data -- 1 for correct, 0 for incorrect
+% Row 2 is the size, in radians, of the change that observers were shown
+% (the difference between color 1 and color 2)
 function p = Mixture2AFCpdf(data, g, k) 
   thetas = vonmisescdf(data(2,:)./2, 0, k).*(1-g) + g/2;
   p = binopdf(data(1,:), 1, thetas);
