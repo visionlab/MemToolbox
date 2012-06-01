f4ecbf54e7ece2128a278d257dccf5fa0693011a
Jordan Suchow <suchow@fas.harvard.edu>
2012-05-31 21:53:55 -0400

    cleanup from switch over to having data.errors in degrees

--- MemModels/StandardMixtureModel.m
+++ MemModels/StandardMixtureModel.m
@@ -7,7 +7,7 @@ function model = StandardMixtureModel()
 	model.lowerbound = [0 0]; % Lower bounds for the parameters
 	model.upperbound = [1 Inf]; % Upper bounds for the parameters
 	model.movestd = [0.02, 0.1];
-	model.pdf = @(data, g, sd) ((1-g).*vonmisespdf(data.errors(:),0,sd2k(sd)) + ...
+	model.pdf = @(data, g, sd) ((1-g).*vonmisespdf(data.errors(:),0,deg2k(sd)) + ...
                                 (g).*unifpdf(data.errors(:),-180,180));
 	model.start = [0.2, 10;  % g, sd
                  0.4, 15;  % g, sd
@@ -19,5 +19,5 @@ end
 % calls the standardmixturemodel generator with mu=0
 function r = StandardMixtureModelGenerator(parameters, dims)
     model = StandardMixtureModelWithBias();
-    r = model.generator({0, parameters{1}, sd2k(parameters{2})}, dims);
+    r = model.generator({0, parameters{1}, deg2k(parameters{2})}, dims);
 end
