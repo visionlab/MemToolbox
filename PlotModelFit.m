65dbee444af04802a9442689bc7a79b4449d18f0
Tim Brady <timothy.brady@gmail.com>
2012-06-02 11:00:25 -0400

    to deal with errors as struct

--- PlotModelFit.m
+++ PlotModelFit.m
@@ -1,10 +1,13 @@
-function PlotModelFit(model, params, data, pdfColor, nBins)
+function PlotModelFit(model, params, data, pdfColor, nBins, showNumbers)
   % Default parameters
-  if nargin < 4
+  if nargin < 4 || isempty(pdfColor)
     pdfColor = 'b';
   end
-  if nargin < 5
+  if nargin < 5 || isempty(nBins)
     nBins = 40;
+  end
+  if nargin <6 || isempty(showNumbers)
+    showNumbers = true;
   end
   
   % If params is a struct, assume they passed a stored() struct from MCMC
@@ -17,16 +20,19 @@ function PlotModelFit(model, params, data, pdfColor, nBins)
     model.pdf = @(varargin)(exp(model.logpdf(varargin{:})));
   end
   
+  if(~isfield(data,'errors'))
+    data = struct('errors',data);
+  end
+  
   % Plot data histogram
   set(gcf, 'Color', [1 1 1]);
-  x = linspace(-pi, pi, nBins)';
-  n = hist(data, x);
+  x = linspace(-180, 180, nBins)';
+  n = hist(data.errors(:), x);
   bar(x, n./sum(n), 'EdgeColor', [1 1 1], 'FaceColor', [.8 .8 .8]);
-  xlim([-pi pi]); hold on;
-  %palettablehistogram;
+  xlim([-180 180]); hold on;
   
   % Plot scaled version of the prediction
-  vals = linspace(-pi, pi, 500)';
+  vals = linspace(-180, 180, 500)';
   multiplier = length(vals)/length(x);
   
   % If params has multiple rows, as if it came from a stored struct, then
@@ -34,7 +40,7 @@ function PlotModelFit(model, params, data, pdfColor, nBins)
   if size(params,1) > 1
     for i=1:size(params,1)
       paramsAsCell = num2cell(params(i,:));
-      p(i,:) = model.pdf(vals, paramsAsCell{:});
+      p(i,:) = model.pdf(struct('errors', vals), paramsAsCell{:});
       p(i,:) = p(i,:) ./ sum(p(i,:));
     end
     bounds = quantile(p, [.05 .50 .95])';
@@ -44,10 +50,10 @@ function PlotModelFit(model, params, data, pdfColor, nBins)
     %set(h, 'LineWidth', 2);
   else
     paramsAsCell = num2cell(params);
-    p = model.pdf(vals, paramsAsCell{:});
+    p = model.pdf(struct('errors', vals), paramsAsCell{:});
     plot(vals, p(:) ./ sum(p(:)) .* multiplier, 'Color', pdfColor, 'LineWidth', 2);
   end
-  xlabel('Error (radians)');
+  xlabel('Error (degrees)');
   ylabel('Probability');
   
   % Always set ylim to 120% of the histogram height, regardless of function
@@ -56,11 +62,11 @@ function PlotModelFit(model, params, data, pdfColor, nBins)
   ylim([0 topOfY]);
   
   % Label the plot with the parameter values
-  if size(params,1) == 1
+  if showNumbers && size(params,1) == 1
     txt = [];
     for i=1:length(params)
       txt = [txt sprintf('%s: %.3g\n', model.paramNames{i}, params(i))];
     end
-    text(pi, topOfY-0.02, txt, 'HorizontalAlignment', 'right');
+    text(180, topOfY-0.02, txt, 'HorizontalAlignment', 'right');
   end
 end
