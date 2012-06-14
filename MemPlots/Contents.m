1e924bfbbd36da16feb35ee6339f6bc50f18f8a0
Tim Brady <timothy.brady@gmail.com>
2012-06-14 12:51:16 -0400

    help files

---
+++ MemPlots/Contents.m
@@ -0,0 +1,8 @@
+% MemToolbox::MemPlots
+%
+% Commands for plotting data and model fits
+%   helpbrowser - Bring up the help browser.
+%   helpdesk    - Comprehensive hypertext documentation and troubleshooting.
+%   helpview    - Display HTML file in help viewer.
+%   doc         - Display HTML documentation in the Help browser.
+%   docsearch   - Search HTML documentation in the Help browser.
