9219656025412fe746268080367e9690df2ea6b4
Tim Brady <timothy.brady@gmail.com>
2012-06-17 00:23:44 -0400

    combine plotting of MCMC and grid search outputs into a single function

--- MemPlots/PlotPosterior.m
+++ MemPlots/PlotPosterior.m
@@ -1,6 +1,19 @@
-function figHand = PlotPosterior(posteriorSamples, paramNames)
+% PlotPosterior - plot results of MCMC or GridSearch; show full posterior
+% 
+function figHand = PlotPosterior(posterior, paramNames)
   % Show 2x2 correlation for each variable with each other to look for
   % structure; Visualize both as a scatter and as a 2D histogram
+  if isfield(posterior, 'vals')
+    % posteriorSamples:
+    figHand = PlotPosterior_MCMC(posterior, paramNames);
+  elseif isfield(posterior, 'likeMatrix')
+    % fullPosterior:
+    figHand = PlotPosterior_GridSearch(posterior, paramNames);
+  end
+end
+
+function figHand = PlotPosterior_MCMC(posteriorSamples, paramNames)  
+  % Plot correlation
   figHand = figure;
   N = length(paramNames);
   for p=1:N
@@ -26,7 +39,46 @@ function figHand = PlotPosterior(posteriorSamples, paramNames)
     ylabel(paramNames{p}, 'FontSize', 15);
   end
   
-  % comestics
+  % Comestics
   colormap(palettablecolormap('sequential'));
   palettablehistogram;
+end
+
+function figHand = PlotPosterior_GridSearch(fullPosterior, paramNames)
+  % Shorten names
+  likeMatrix = fullPosterior.likeMatrix;
+  valuesUsed = fullPosterior.valuesUsed;
+  
+  % Show 2x2 correlation for each variable with each other to look for
+  % structure; Visualize both as a scatter and as a 2D histogram  
+  figHand = figure;
+  N = length(paramNames);
+  for p=1:N
+    for p2=1:(p-1)
+      subplot(N,N,sub2ind([N N],p,p2));
+      V = ndsum(likeMatrix,  find(1:N ~= p & 1:N ~= p2));
+      imagesc(valuesUsed{p}, valuesUsed{p2}, V);
+      set(gca,'YDir','normal');
+      axis tight;
+      
+      subplot(N,N,sub2ind([N N],p2,p));
+      imagesc(valuesUsed{p2}, valuesUsed{p}, V');
+      set(gca,'YDir','normal');
+      axis tight;
+    end
+    
+    subplot(N,N,sub2ind([N N],p,p));
+    marginal = ndsum(likeMatrix, find(1:N ~= p)); 
+    bar(valuesUsed{p}, marginal);
+    axis tight;
+    set(gca, 'YTick', []);
+    subplot(N,N,sub2ind([N N],p,1));
+    title(paramNames{p}, 'FontSize', 15);
+    subplot(N,N,sub2ind([N N],1,p));
+    ylabel(paramNames{p}, 'FontSize', 15);
+  end
+  
+  % Comestics
+  colormap(palettablecolormap('sequential'));
+  palettablehistogram();
 end
