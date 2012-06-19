b20b3d368826bcbdba22ef9653174cf63c0fd9fd
Jordan Suchow <suchow@fas.harvard.edu>
2012-06-19 10:39:01 -0400

    drop in replacements for hist and plot with sensible defaults

---
+++ MemPlots/Palettable/pplot.m
@@ -0,0 +1,15 @@
+function phist(varargin)
+  
+  opengl('OpenGLLineSmoothingBug',1)
+  
+  plot(varargin{:}, 'Color', [0.54, 0.61, 0.06], 'LineWidth', 1.25);
+  
+  % make the background white
+  set(gcf,'Color',[1 1 1]);
+  
+  % kill the upper and right axis
+  set(gca, 'box', 'off');  
+  
+  % bring the axis to front
+  set(gca,'layer','top');
+end
