dc186e659cecb90e5b376bf3ed5da2d514eae2da
Tim Brady <timothy.brady@gmail.com>
2012-06-19 11:39:12 -0400

    spacing

--- MemPlots/Palettable/palettablecolors.m
+++ MemPlots/Palettable/palettablecolors.m
@@ -4,12 +4,12 @@ function colors = palettablecolors(n)
         n = 1;
     end
     
-    allColors = [233,127,2;  % party confetti
-                 189,21,80;  % sugar hearts you
-                  73,10,61;  % sugar cocktail
-                 11,72,107;  % adrift in dreams
-								138,155,15;  % happy balloon
-									 3,54,73;];% acqua profonda 
+    allColors = [233,127,2;   % party confetti
+                 189,21,80;   % sugar hearts you
+                  73,10,61;   % sugar cocktail
+                 11,72,107;   % adrift in dreams
+								138,155,15;   % happy balloon
+									 3,54,73;]; % acqua profonda 
 	               
 	colors = allColors(1 + mod(0:n-1, length(allColors)),:)./255;
 end
