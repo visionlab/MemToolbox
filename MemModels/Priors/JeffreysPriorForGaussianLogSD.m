b23fdce03f0bb24cad56485fed9b0329cb840e7f
Jordan Suchow <suchow@fas.harvard.edu>
2012-07-17 17:33:15 -0400

    vectorize priors

--- MemModels/Priors/JeffreysPriorForGaussianLogSD.m
+++ MemModels/Priors/JeffreysPriorForGaussianLogSD.m
@@ -1,4 +1,4 @@
 % x can be either log sd or log variance
 function y = JeffreysPriorForGaussianLogSD(x)
-  y = 1/(x^2);
+  y = 1./(x^2);
 end
--- MemModels/Priors/JeffreysPriorForKappaOfVonMises.m
+++ MemModels/Priors/JeffreysPriorForKappaOfVonMises.m
@@ -1,4 +1,4 @@
 function y = JeffreysPriorForKappaOfVonMises(K)
-  z = besseli(1,K) / besseli(0,K);
-  y = z * (K - z - K*z^2);
+  z = besseli(1,K) ./ besseli(0,K);
+  y = z .* (K - z - K.*z.^2);
 end
