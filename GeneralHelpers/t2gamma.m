42ade56b367ed396b33e965f2cbb231e3f8c9820
Jordan Suchow <suchow@fas.harvard.edu>
2012-07-18 20:39:39 -0400

    converts from student's t parameters to gamma mixing distribution parameters

---
+++ GeneralHelpers/t2gamma.m
@@ -0,0 +1,8 @@
+% converts the parameters of a student's t distribution (degrees of freedom df
+% and standard deviation sd) to the corresponding gamma distribution that
+% produces it when used as a mixing distribution for a scale mixture of 
+% normals.
+function [shape,scale] = t2gamma(sd,df)
+  shape = df./2;
+  scale = 1 ./ (df .* sd.^2 ./ 2);
+end
