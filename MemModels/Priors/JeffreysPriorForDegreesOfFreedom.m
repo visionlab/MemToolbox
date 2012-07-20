f184928fed75b75a5da5b74bf89d16e42465f942
Tim Brady <timothy.brady@gmail.com>
2012-07-19 21:01:22 -0400

    initial commit of new models

---
+++ MemModels/Priors/JeffreysPriorForDegreesOfFreedom.m
@@ -0,0 +1,11 @@
+function y = JeffreysPriorForDegreesOfFreedom(df)
+    
+  term1 = df./(df+3);
+  
+  term2a = trigamma(df./2);
+  term2b = trigamma((df+1)./2);
+  term2c = (2.*(df+3))./(df.*((df+1).^2));
+
+  y = sqrt(term1) .* sqrt(term2a - term2b - term2c);
+
+end
--- MemModels/SlotPlusResourcesModel.m
+++ MemModels/SlotPlusResourcesModel.m
@@ -1,4 +1,4 @@
-% SLOTMODEL returns a structure for a two-component mixture model with 
+% SLOTPLUSRESOURCESMODEL returns a structure for a two-component mixture model with 
 % capacity K and precision sd.
 %
 % Parameter explanation: 
@@ -8,19 +8,19 @@
 % For example, if participants can store 3 items but have to remember 6, 
 % participants will guess 50% of the time. 
 %
-% Precision is the uncertainty of a single slot. When there are more slots than
-% items the precision of the stored representation increases through a process 
-% called averaging (Shaw, 1980). 
+% Precision is the maximum uncertainty. When set size is less than the
+% upper limit on storage, precision can be improved by averaging (Shaw, 1980) 
+% (pooling resources to an item)
 %
-% SLOTMODEL uses the capacity and precision to fit data across multiple sizes. 
+% Uses the capacity and precision to fit data across multiple sizes. 
 %
 % TO DO
 %   TEST!!
 %   Make a version that fits at one set size and generates to a novel set size
 %   Make this a default option in the tutorial to highlight the issues with it
 
-function model = SlotPlusAveragingModel()
-  model.name = 'Slot model';
+function model = SlotPlusResourcesModel()
+  model.name = 'Slot plus resouces model';
 	model.paramNames = {'capacity', 'sd'};
 	model.lowerbound = [0 0]; % Lower bounds for the parameters
 	model.upperbound = [Inf Inf]; % Upper bounds for the parameters
@@ -34,7 +34,7 @@ end
 function y = slotpdf(data,capacity,sd)
   g = (1 - max(0,min(1,capacity./data.n)));
 
-  y = (1-g).*vonmisespdf(data.errors(:),0,deg2k( min(sd/sqrt(capacity./data.n),sd))) + ...
+  y = (1-g).*vonmisespdf(data.errors(:),0,deg2k( min(sd./sqrt(capacity./data.n),sd))) + ...
         (g).*unifpdf(data.errors(:),-180,180);
    
 end
