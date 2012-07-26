5e15fd38939dd6ecc893d5d18c41567a7144bdf7
Jordan Suchow <suchow@fas.harvard.edu>
2012-07-26 18:31:32 -0400

    new plot and restructuring of palettable toolbox

---
+++ MemPlots/Palettable/makepalettable.m
@@ -0,0 +1,22 @@
+% MAKEPALETTABLE Overrides some of MATLAB's defaults to make a figure that
+% is at least palettable.
+function makepalettable(f)
+  
+  if nargin < 1
+    f = gcf();
+  end
+
+  % make the background white
+  set(f,'Color',[1 1 1]);
+  
+  % kill the upper and right axis
+  set(get(f,'CurrentAxes'), 'box', 'off');
+  
+  % make the bars grey with small white borders
+  h = findobj(f,'Type','patch');
+  set(h,'FaceColor',[0.7 0.7 0.7],'EdgeColor',[1 1 1]);
+  
+  % bring the axis to front
+  set(get(f,'CurrentAxes'),'layer','top');
+  
+end
---
+++ MemPlots/PlotShift.m
@@ -0,0 +1,55 @@
+% DOTSHIFTPLOT Plots shift in parameter values between two conditions, for 
+% each subject and for the group average.
+%
+%   Sample usage:
+%
+%   cond1 = rand(10,3); % rows are parameters, columns are subjects
+%   cond2 = rand(10,3)
+%   PlotShift(cond1,cond2)
+%
+function PlotShift(cond1,cond2,paramNames)
+  
+  if nargin < 3
+    for i = 1:size(cond1,2)
+      paramNames{i} = ['Parameter ' num2str(i)];
+    end
+  end
+  
+  f = figure();
+  
+  numParameters = size(cond1,2);
+  combos = combnk([1:numParameters],2);
+  numCombos = size(combos,1);
+  
+  for i = 1:numCombos
+    
+    p1 = combos(i,1);
+    p2 = combos(i,2);
+    
+    subplot(1,numCombos,i)
+    hold on;
+  
+    % plot subject changes
+    for j = 1:size(cond1,1)
+      line([cond1(j,p1) cond2(j,p1)], [cond1(j,p2) cond2(j,p2)], ... 
+        'LineStyle', '-', 'Color', [0.85,0.85,0.85])
+    end
+    plot(cond1(:,p1), cond1(:,p2), 'MarkerSize', 18, 'LineStyle', '.', ...
+      'Color', [0.75 0.75 0.75]);
+    plot(cond2(:,p1), cond2(:,p2), 'MarkerSize', 18, 'LineStyle', '.', ...
+      'Color', [0.3 0.3 0.3]);
+    
+    % plot mean changes 
+    line([mean(cond1(:,p1)) mean(cond2(:,p1))], [mean(cond1(:,p2)) mean(cond2(:,p2))], ...
+      'Color', [0.0432, 0.4098, 0.5098], 'LineWidth', 1.5)
+    plot(mean(cond1(:,p1)),mean(cond1(:,p2)), 'LineStyle', '.', 'MarkerSize', 32, ...
+      'Color', [0.0863, 0.5765, 0.6471])
+    plot(mean(cond2(:,p1)),mean(cond2(:,p2)), 'LineStyle', '.', 'MarkerSize', 32, ...
+      'Color', [0, 0.2431, 0.3725])
+      
+    xlabel(paramNames{p1});
+    ylabel(paramNames{p2});
+    
+    makepalettable(f);
+  end
+end
