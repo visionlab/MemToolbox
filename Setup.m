bb34f722d2babac043e707350f116f289ca36ec4
Tim Brady <timothy.brady@gmail.com>
2012-07-26 01:05:57 -0400

    don't add .git if it exists

--- Setup.m
+++ Setup.m
@@ -2,6 +2,7 @@
 % copy any files -- you must first put MemToolbox where you want it to 
 % be located, then run this file to add the files to the path.
 addpath(genpath(pwd));
+rmpath(genpath(fullfile(pwd, '.git')));
 if savepath() == 0
   fprintf(['\nMemToolbox successfully added to PATH!\n' ...
     'You are ready to go!\n\n' ...
