c01e356e6fbc04975a4e937af60248f8ca425d3d
Jordan Suchow <suchow@fas.harvard.edu>
2012-07-26 11:52:18 -0400

    new syntax for zero-parameter models

--- MemModels/AllGuessingModel.m
+++ MemModels/AllGuessingModel.m
@@ -3,14 +3,11 @@
 % remembered state.
 function model = AllGuessingModel()
   model.name = 'All guessing model';
-	model.paramNames = {'g'};
-	model.lowerbound = 1; % Lower bounds for the parameters
-	model.upperbound = 1; % Upper bounds for the parameters
-	model.movestd = 0.00;
-	model.pdf = @(data, g) 1*unifpdf(data.errors(:),-180,180);
-	model.start = [1;  % g
-                 1;  % g
-                 1]; % g
-  
-  model.generator = @(parameters,dims) (unifrnd(-180,180,dims));
+	model.paramNames = {};
+	model.lowerbound = []; % Lower bounds for the parameters
+	model.upperbound = []; % Upper bounds for the parameters
+	model.movestd = [];
+	model.pdf = @(data) 1*unifpdf(data.errors(:),-180,180);
+	model.start = []; % g
+  model.generator = @(parameters,dims,varargin) (unifrnd(-180,180,dims));
 end
