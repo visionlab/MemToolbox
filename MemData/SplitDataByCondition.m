dc73d809d899d3ffe77e5ab8241645a9369e8b55
Tim Brady <timothy.brady@gmail.com>
2012-08-11 12:51:18 -0400

    allow condition to be a cell array of strings, and also fix a bug with data.distractors being messed up

--- MemData/SplitDataByCondition.m
+++ MemData/SplitDataByCondition.m
@@ -1,11 +1,20 @@
-% SPLITDATABYCONDITION Splits a data set into subsets, one per condition, as
-% specified by a field data.condition.
+% SPLITDATABYCONDITION Splits a data set into subsets, one per condition
+% as specified by a field data.condition. data.condition can be a cell 
+% array of strings or a vector. 
 %
-% data = MemDataset(4);
+% e.g.,
+%    data.errors =    [10 30 -30 20 -12 80];
+%    data.condition = [1  1   1  2   2  2];
+%    datasets = SplitDataByCondition(data)
+%
+% or
+%    data.errors =    [10   30   -30  20   -12  80];
+%    data.condition = {'a', 'a', 'a', 'b', 'b', 'b'};
+%    datasets = SplitDataByCondition(data)
 %
 function [datasets, conditionOrder] = SplitDataByCondition(data)
   
-  % if there is no condition field, return the data struct untouched
+  % If there is no condition field, return the data struct untouched
   if(~isfield(data, 'condition'))
     warning('The data does not specify conditions.')
     datasets = {data};
@@ -13,18 +22,31 @@ function [datasets, conditionOrder] = SplitDataByCondition(data)
     return;
   end
 
-  % otherwise figure out how many conditions there are
+  % Otherwise figure out how many conditions there are
   conditions = unique(data.condition);
-  datasets = cell(1,length(conditions));
+  [conditionOrder, tmp, condNumbers] = unique(data.condition);
+  nConds = max(condNumbers);
+  datasets = cell(1,nConds);
 
-  % for each field, split it by condition and store it
+  % For each field, split it by condition and store it
   fields = fieldnames(data);
-  for condIndex = 1:length(conditions)
-    cond = conditions(condIndex);
+  for condIndex = 1:nConds
     for fieldIndex = 1:length(fields)
       wholeField = getfield(data,fields{fieldIndex});
-      datasets{condIndex} = setfield(datasets{cond}, fields{fieldIndex}, ...
-        wholeField(find(data.condition == cond)));
+      
+      % Preserve all rows of fields like .distractors that are M x trials
+      % and allow them to also be trials X M
+      if size(wholeField, 1) == length(condNumbers)
+        datasets{condIndex} = setfield(datasets{condIndex}, fields{fieldIndex}, ...
+          wholeField(condNumbers == condIndex, :));
+      elseif size(wholeField, 2) == length(condNumbers)
+        datasets{condIndex} = setfield(datasets{condIndex}, fields{fieldIndex}, ...
+          wholeField(:, condNumbers == condIndex));
+      else
+        fprintf('Warning: Could not split field "%s"!\n', fields{fieldIndex});
+        datasets{condIndex} = setfield(datasets{condIndex}, fields{fieldIndex}, ...
+          wholeField);
+      end
     end 
   end
 end
