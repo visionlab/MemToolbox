d6d0924f71fc1d52b35576bf9eaab488f16fd6ee
Tim Brady <timothy.brady@gmail.com>
2012-08-10 23:56:19 -0400

    Wrapper function to allow bias terms; add ability to specify a prior on mu

--- MemModels/WithBias.m
+++ MemModels/WithBias.m
@@ -1,8 +1,12 @@
 % WITHBIAS adds a bias terms to any model 
 %
-%  model = WithBias(model)
+%  model = WithBias(model, [priorForMu])
 %
-% e.g., to take the StandardMixtureModel, which has a guess rate (g) and
+% The first parameter is the model to convert; the second (optional)
+% parameter is a function for the prior on mu. It takes a single argument,
+% mu.
+%
+% Thus, to take the StandardMixtureModel, which has a guess rate (g) and
 % standard deviation (sd), and add a shift term (mu), just use:
 %   model = WithBias(StandardMixtureModel())
 %
@@ -10,7 +14,12 @@
 % example, the following works fine:
 %   model = TwoAFC(WithBias(StandardMixtureModel());
 %
-function model = WithBias(model)
+function model = WithBias(model, priorForMu)
+  % If no prior is specified, default to an improper uniform prior
+  if nargin < 2
+    priorForMu = @(p)(1);
+  end
+  
   % Take model and turn it into a model with a bias term
   model.name = [model.name ' with bias'];
   model.paramNames = {'mu', model.paramNames{:}};
@@ -19,12 +28,17 @@ function model = WithBias(model)
   model.movestd = [1 model.movestd];
   model.start = [rand(size(model.start,1),1)*10  model.start];
   
+  % Adjust pdf and prior
   model.oldPdf = model.pdf;
   model.pdf = @NewPDF;
+  model.oldPrior = model.prior;
+  model.prior = @(p)(model.prior(p(2:end)) .* priorForMu(p(1)));
   
-  % Convert orientation data to a format that is useable in all the models
+  % Shift errors and/or changeSize 
   function p = NewPDF(data, mu, varargin)
-    data.errors = wrap(data.errors - mu);
+    if isfield(data, 'errors')
+      data.errors = wrap(data.errors - mu);
+    end
     if isfield(data, 'changeSize')
       data.changeSize = wrap(data.changeSize - mu);
     end
