32f1a718ec723c11b90b4d90274c6475d8c13075
Tim Brady <timothy.brady@gmail.com>
2012-08-10 22:08:38 -0400

    bayes factors based on importance sampling from t-dist

--- MemModelComparison/ModelComparison_BayesFactor.m
+++ MemModelComparison/ModelComparison_BayesFactor.m
@@ -1,66 +1,95 @@
 % MODELCOMPARISON_BAYESFACTOR Compute Bayes Factor via Monte Carlo
 % 
+%  [logBayesFactor,logPosteriorOdds,posteriorOdds] = ...
+%             ModelComparison_BayesFactor(data, models, [optionalParameters])
+%
 % A Bayes factor is computed by asking how likely the data that was observed
 % is, given the entire set of data the model can possibly predict. This 
-% penalizes complex models -- having extra freedom means they can predict
+% penalizes complex models: having extra freedom means they can predict
 % more different kinds of data, and thus each particular set of data is
 % less likely. 
 %
-%  Log Bayes Factors can be interpreted as follows: (per Jeffreys (1961))
+%  Log Bayes Factors can be interpreted as follows: (per Jeffreys, 1961)
 %   0 to 0.5  - not worth more than a mention
 %   0.5 to 1  - substantial
 %   1 to 2    - strong
 %   >2        - decisive
 %
-% To compute a Bayes Factor we use Monte Carlo. Thus we draw samples from the
-% prior of the model (e.g., choose 'random' values of its parameters), and
-% then see how likely the data is under those parameters. Models that
+% To compute a Bayes Factor we use Monte Carlo. Thus we draw samples from 
+% the prior of the model (e.g., choose 'random' values of its parameters), 
+% and then see how likely the data is under those parameters. Models that
 % predict the data well over their entire possible space of parameters do
 % well, and models that need very particular settings of their parameters
-% to fit the data do poorly. This approximation method is quite accurate
-% for small numbers of parameters (<~5) as in most models in use in
-% MemToolbox.
+% to fit the data do poorly. This approximation method for computing Bayes
+% Factors is relatively accurate for small numbers of parameters (<~5) as  
+% in most models in use in MemToolbox; in particular, it is accurate enough
+% for the scale above (log units). However, Bayes factors should not be 
+% considered to be precise.
 %
-% Examples:
-% 
+% Computing a Bayes Factor depends on the prior of the model, since that is
+% what specified how flexible the model is -- what data you could have seen
+% if you generated it from this model. Thus, ModelComparison_BayesFactor
+% makes use of a different, less diffuse prior, model.priorForMC, that you
+% should specify for each of your models. In specifying this prior, you may
+% want to make use of the PlotSamplesFromPrior function to visualize the
+% prior you have specified.
+%
+%
+% Example usage:
 %  Take MemDataset(3), which in reality contains 20% 'swaps' with
 %  distractors.
 %  
 %   data = MemDataset(3);
-%   bf = ModelComparison_BayesFactorMonteCarlo(data, {StandardMixtureModel(), SwapModel()})
+%   bf = ModelComparison_BayesFactor(data, {StandardMixtureModel(), SwapModel()})
 %  
 %  Posterior odds of models:
 %    0.00	  1.00	
 %
 %    bf = 
-%            0      -46.489
-%       46.489            0
+%            0      -49.045
+%       49.045            0
 %
-%  This suggests entirely definitively that the SwapModel provides a better
-%  fit, with a Bayes Factor preference of model 2 over model 1 (bf(2,1)) of
-%  46.
+%  This suggests definitively that the SwapModel provides a better fit,
+%  with a Bayes Factor preference of model 2 over model 1 (bf(2,1)) of
+%  49.
 %
 %  Now scramble the distractors, meaning there are very few 'swaps':
 %
 %   data.distractors = Shuffle(data.distractors')';
-%   bf = ModelComparison_BayesFactorMonteCarlo(data, {StandardMixtureModel(), SwapModel()})
+%   bf = ModelComparison_BayesFactor(data, {StandardMixtureModel(), SwapModel()})
 %
 %  Posterior odds of models:
-%     0.84	  0.16	
+%     0.97	  0.03	
 %
 %    bf =
-%            0      0.71015
-%     -0.71015            0
+%            0       1.4581
+%      -1.4581            0
 %
 % With no actual swaps in the data, the same process now reveals
-% substantial evidence for the StandardMixtureModel rather than the swap
+% strong evidence for the StandardMixtureModel rather than the swap
 % model. This is because the SwapModel is penalized for its complexity
-% (all settings of B>0 result in poor fits to the data).
+% (all settings with swaps (e.g., B>0) result in poor fits to the data).
 %
-
-%---------------------------------------------------------------------
-function [logBayesFactor,logPosteriorOdds,posteriorOdds] = ModelComparison_BayesFactor(data, models, varargin)
-  args = struct('Verbosity', 1);
+% Optional parameters:
+%  'PosteriorSamples' - Calculating the Bayes Factors is done by using a 
+%  t-distribution to approximate the posterior to choose the range of samples
+%  from the prior. This requires running MCMC on each model, which in many
+%  cases you will have already done. Thus, the function takes an optional
+%  parameter 'PosteriorSamples', which is a cell array the same
+%  length as the cell array of models that contains these samples. 
+%
+%  'NumSamples' - how many samples from the prior to take in order to
+%  estimate the Bayes Factor. The default is 100,000. Increase this if you
+%  get different Bayes Factors on subsequent runs of the same model
+%  comparison, or if your model is particularly complex:
+%
+%    bf = ModelComparison_BayesFactor(data, {StandardMixtureModel(), ...
+%           SwapModel()}, 'NumSamples', 150000);
+%
+function [logBayesFactor,logPosteriorOdds,posteriorOdds] = ...
+    ModelComparison_BayesFactor(data, models, varargin)
+  
+  args = struct('Verbosity', 2, 'NumSamples', 100000, 'PosteriorSamples', []);
   args = parseargs(varargin, args);
   
   if length(models) < 2
@@ -70,41 +99,72 @@ function [logBayesFactor,logPosteriorOdds,posteriorOdds] = ModelComparison_Bayes
   % Make sure each of the models are well formed
   for m = 1:length(models)
     if ~isfield(models{m}, 'priorForMC')
-      fprintf(['WARNING: Using a Bayes Factor with a very difuse prior, like ' ...
-        'the defaults used for interference by MemToolbox, is nearly meaningless. It '...
-        'will *heavily* penalize the model with more parameters. Try setting ' ...
-        'a priorForMC() function on your model with more reasonable priors.\n\n']);
+      fprintf(['WARNING: No priorForMC found in model ' num2str(m) '!\n' ...
+        'Bayes Factors, unlike estimation of parameters, \n' ...
+        'are heavily influenced by your prior. Many of the models in \n'...
+        'MemToolbox are specified with very diffuse priors, which \n'...
+        'will heavily penalize models with more parameters. You will \n'...
+        'probably want to set more reasonable priors (e.g., if your set \n'...
+        'size is 2, your guess rate will probably be 0.1-0.4) before \n'...
+        'examining Bayes Factors. To do so, specify a priorForMC()\n'...
+        'function on your model. This prior will only be used in \n'...
+        'calculating Bayes Factors, not doing estimation of parameters.\n']);
     end
     models{m} = EnsureAllModelMethods(models{m});
     models{m}.prior = models{m}.priorForMC;
-  end
-  
-  % Get samples from prior
-  if args.Verbosity > 0
-    fprintf('\nComparing the %d models...\n', length(models));
-  end
-  for m = 1:length(models)
-    if args.Verbosity > 0
-      fprintf('  Sampling from prior of model %d: %s\n', m, models{m}.name);
+    models{m}.logprior = @(p) sum(log(models{m}.priorForMC(p)));
+    if isempty(args.PosteriorSamples)
+      if args.Verbosity > 0
+        fprintf('  Calculating posterior samples for model %d: %s\n', m, models{m}.name);
+      end
+      posteriorSamples{m} = MCMC(data, models{m}, ...
+        'Verbosity', 0, 'PostConvergenceSamples', 1000);
     end
-    priorModel = models{m};
-    priorModel.pdf = @(data, varargin)(1);
-    priorModel.logpdf = @(data, varargin)(0);
-    priorSamples{m} = MCMC([], priorModel, 'Verbosity', 0, 'PostConvergenceSamples', 10000);
   end
   
+  % Compute importance functions
+  for m=1:length(models)
+    centerGaussian{m} = mean(posteriorSamples{m}.vals);
+    covarGaussian{m} = cov(posteriorSamples{m}.vals); 
+  end
+  
+  % Get samples from importance function
+  for m=1:length(models)
+    % mvtrnd converts to the cov matrix to a correlation matrix, so we have
+    % to adjust the variances (and mean) later...
+    importanceSamples{m} = mvtrnd(covarGaussian{m}, 5, args.NumSamples);
+    rowBad = zeros(size(importanceSamples{m},1),1);
+    for i=1:size(importanceSamples{m},2)
+      % adjust variance/mean
+      importanceSamplesRescaled{m}(:,i) = ...
+        importanceSamples{m}(:,i).*sqrt(covarGaussian{m}(i,i)) + centerGaussian{m}(i);
+      
+      % throw out samples outside the bounds
+      rowBad = rowBad | ...
+        importanceSamplesRescaled{m}(:,i)>=models{m}.upperbound(i) | ...
+        importanceSamplesRescaled{m}(:,i)<=models{m}.lowerbound(i);
+    end
+    if sum(rowBad)>length(rowBad)*0.20
+      fprintf('  Warning: many samples were outside range of function, only using %d samples.\n', ...
+        sum(rowBad));
+    end
+    importanceSamples{m}(rowBad,:) = [];
+    importanceSamplesRescaled{m}(rowBad,:) = [];
+  end
+  
+  % Calculate likelihood
   for m = 1:length(models)    
-    % Bayes factor is the average likelihood of 
-    % each model, weighted by the prior.
     if args.Verbosity > 0
       fprintf('  Calculating likelihoods for model %d: %s\n', m, models{m}.name);
     end
-    for i=1:size(priorSamples{m}.vals,1)
-      params = num2cell(priorSamples{m}.vals(i,:));
+    parfor i=1:size(importanceSamples{m},1)
+      logW(i) = models{m}.logprior(importanceSamplesRescaled{m}(i,:)) ...
+        - log(mvtpdf(importanceSamples{m}(i,:), covarGaussian{m}, 5));
+      params = num2cell(importanceSamplesRescaled{m}(i,:));
       logLike(i) = models{m}.logpdf(data, params{:});
     end
-    summedLogLike = logsumexp(logLike'); % Returns log(sum(exp(a)))
-    logAverageLike = summedLogLike -  log(size(priorSamples{m}.vals,1));
+    summedLogLike = logsumexp(logLike' + logW'); % Returns log(sum(exp(a)))
+    logAverageLike = summedLogLike -  logsumexp(logW');
     logPosteriorOdds(m) = logAverageLike / log(10); % Convert to log base 10
   end
   
@@ -112,7 +172,7 @@ function [logBayesFactor,logPosteriorOdds,posteriorOdds] = ModelComparison_Bayes
   posteriorOdds = 10.^(logPosteriorOdds - max(logPosteriorOdds));
   posteriorOdds = posteriorOdds ./ sum(posteriorOdds);
   
-  if args.Verbosity > 0
+  if args.Verbosity > 1
     fprintf('\n  Posterior odds of models:\n    ');
     fprintf('%0.2f\t', posteriorOdds(:));
     fprintf('\n\n');
