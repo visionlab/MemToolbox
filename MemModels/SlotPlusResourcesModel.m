75d8c9e5218f8cdf3187546da041fcdc026628f6
Tim Brady <timothy.brady@gmail.com>
2012-08-12 21:59:58 -0400

    custom model plots that show fits for each set size

--- MemModels/SlotPlusResourcesModel.m
+++ MemModels/SlotPlusResourcesModel.m
@@ -1,7 +1,11 @@
 % SLOTPLUSRESOURCESMODEL returns a structure for a two-component mixture 
 % model with capacity K and precision sd.
 %
-% In addition to data.errors, requires data.n (the set size for each trial)
+% In addition to data.errors, requires data.n (the set size for each
+% trial). The model is not particularly well-formed unless you have tested
+% multiple set sizes; with only a single set size you may be better off
+% with a model that does not make predictions across set size, like
+% StandardMixtureModel().
 %
 % Parameter explanation: 
 %
@@ -12,7 +16,7 @@
 %
 % Precision is the maximum uncertainty. When set size is less than the
 % upper limit on storage, precision can be improved by averaging (Shaw, 1980) 
-% e.g., by pooling resources to an item.
+% e.g., by pooling resources to an item. 
 %
 % Uses the capacity and precision to fit data across multiple sizes. 
 %
@@ -21,13 +25,37 @@ function model = SlotPlusResourcesModel()
 	model.paramNames = {'capacity', 'sd'};
 	model.lowerbound = [0 0]; % Lower bounds for the parameters
 	model.upperbound = [Inf Inf]; % Upper bounds for the parameters
-	model.movestd = [1, 0.1];
+	model.movestd = [0.1, 0.1];
 	model.pdf = @slotpdf;
   model.prior = @(p) (JeffreysPriorForCapacity(p(1)) .* ... % for capacity
                       JeffreysPriorForKappaOfVonMises(deg2k(p(2))));  
 	model.start = [2, 10;  % g, sd
                  3, 15;  % g, sd
                  4, 20]; % g, sd
+  
+  % Use our custom modelPlot to make a plot of errors separately for each
+  % set size
+  model.modelPlot = @model_plot;
+  function figHand = model_plot(data, params, varargin)
+    figHand = figure();
+    if isstruct(params) && isfield(params, 'vals')
+      params = MCMCSummarize(params, 'maxPosterior');
+    end
+    data.condition = data.n;
+    [datasets, setSizes] = SplitDataByCondition(data);
+    m = StandardMixtureModel();
+    for i=1:length(setSizes)
+      subplot(1, length(setSizes), i);
+      g = (1 - max(0,min(1,params(1)/setSizes(i))));
+      curSD = min(params(2)./sqrt(params(1)./setSizes(i)), params(2));
+      PlotModelFit(m, [g curSD], datasets{i}, 'NewFigure', false, ...
+        'ShowNumbers', true, 'ShowAxisLabels', false);
+      if i==1
+        ylabel('Probability', 'FontSize', 14);
+      end
+      title(sprintf('Set size %d', setSizes(i)), 'FontSize', 14);
+    end
+  end              
 end
 
 function y = slotpdf(data,capacity,sd)
