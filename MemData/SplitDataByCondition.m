c169ea49035cb652a570301f964c25eae50ff8a8
Tim Brady <timothy.brady@gmail.com>
2012-08-12 21:59:39 -0400

    better comments

--- MemData/SplitDataByCondition.m
+++ MemData/SplitDataByCondition.m
@@ -2,6 +2,8 @@
 % as specified by a field data.condition. data.condition can be a cell 
 % array of strings or a vector. 
 %
+%  [datasets, conditionOrder] = SplitDataByCondition(data)
+%
 % e.g.,
 %    data.errors =    [10 30 -30 20 -12 80];
 %    data.condition = [1  1   1  2   2  2];
