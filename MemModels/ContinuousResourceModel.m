608f642ffe4d9de76b1274cf78e6077da7c08143
Jordan Suchow <suchow@fas.harvard.edu>
2012-08-14 01:54:58 -0400

    first commit of continuous resource and slots+averaging model
    the slots+averaging model has a bug when calling model comparison that
    needs to be checked out. details in the comments.

---
+++ MemModels/ContinuousResourceModel.m
@@ -0,0 +1,33 @@
+% CONTINUOUSRESOURCEMODEL returns a structure for a continuous resource model,
+% very much like the one from Bays & Husain (2008).
+%
+% Usage:
+%   data = load('slot-model-simulate.mat');
+%   MemFit(data, ContinuousResourceModel);
+%
+function model = ContinuousResourceModel()
+  model.name = 'Continuous resource model';
+  model.paramNames = {'lapse','k', 'bestSD'};
+  model.lowerbound = [0 0 0]; % Lower bounds for the parameters
+  model.upperbound = [1 10 50]; % Upper bounds for the parameters
+  model.movestd = [0.02, 0.1, 1];
+  model.pdf = @crpdf;
+  model.start = [0.2, 0.1, 10;  % lapse, k, bestSD
+                 0.4, 1, 2;
+                 0.1, 10, 20];
+
+  model.prior = @(p) JeffreysPriorForKappaOfVonMises(deg2k(p(3))) .* ...
+                     JeffreysPriorForCapacity(p(2));
+  
+end
+
+function y = crpdf(data,lapse,k,bestSD)
+
+  propResources = 1./data.n;
+  pMax = 1./(bestSD.^2);
+  precision = (propResources .^ k) * pMax;
+  sd = sqrt(1./precision);
+
+  y = (1-lapse) .* vonmisespdf(data.errors(:),0,deg2k(sd)) + ...
+        (lapse) .* 1/360;
+end
