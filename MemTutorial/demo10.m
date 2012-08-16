647beea44759d12367f10eed55563fa298562ef5
Jordan Suchow <suchow@fas.harvard.edu>
2012-08-16 12:52:09 -0400

    fixes #3

--- MemTutorial/demo10.m
+++ MemTutorial/demo10.m
@@ -1,2 +1,3 @@
+clear data
 data.errors = [-12 3 38 27 -29 21 -22 52 -2 -19 21 17 38 6 34 25 44];
-MemFit(data, Orientation(WithBias(StandardMixtureModel)))
+MemFit(data, Orientation(WithBias(StandardMixtureModel), [1,3]))
