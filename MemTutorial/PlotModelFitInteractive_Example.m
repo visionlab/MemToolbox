549090419d7761347c57720d816ba53fa7e2cd92
Tim Brady <timothy.brady@gmail.com>
2012-08-16 15:27:07 -0400

    fix examples that still make sense

--- MemTutorial/PlotModelFitInteractive_Example.m
+++ MemTutorial/PlotModelFitInteractive_Example.m
@@ -1,5 +1,5 @@
 function PlotModelFitInteractive_Example()
-  data = MemData(1);
-  model = StandardMixtureModelWithBias();
-  PlotModelFitInteractive(model, [0 .5 10], data);
+  data = MemDataset(1);
+  model = WithBias(StandardMixtureModel);
+  PlotModelFitInteractive(model, [0, .5, 10], data);
 end
