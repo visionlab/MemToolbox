e861f72711b5a6a9222b7cde03ecc6f71d2093c2
Tim Brady <timothy.brady@gmail.com>
2012-08-17 03:49:42 -0400

    prevent overflows/nans

--- MemPlots/Palettable/fade.m
+++ MemPlots/Palettable/fade.m
@@ -1,9 +1,12 @@
 %FADE Takes an RGB color and fades it
 function c = fade(color, percent)
-    colorHSV = rgb2hsv(color);
-    colorHSV(2) = colorHSV(2).*percent;
-    colorHSV(3) = colorHSV(3)+(1-percent)/3.5;
-    c = hsv2rgb(colorHSV);
-    c(c<0) = 0;
-    c(c>1) = 1;
+  if isnan(percent)
+    percent = 0.01;
+  end
+  colorHSV = rgb2hsv(color);
+  colorHSV(2) = colorHSV(2).*percent;
+  colorHSV(3) = colorHSV(3)+(1-percent)/3.5;
+  c = hsv2rgb(colorHSV);
+  c(c<0) = 0;
+  c(c>1) = 1;
 end
