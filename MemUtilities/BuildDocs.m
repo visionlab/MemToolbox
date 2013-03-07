68e5eec871fc046ff66ec51d1ab3b88d96b99e27
Jordan Suchow <suchow@fas.harvard.edu>
2013-03-07 18:02:39 -0500

    option to use BuildDocs on a specific subfolder

--- MemUtilities/BuildDocs.m
+++ MemUtilities/BuildDocs.m
@@ -1,11 +1,14 @@
-function BuildDocs()
+function BuildDocs(dir)
+  if(nargin < 1)
+    dir = '**';
+  end
   parentFolder = 'MemUtilities';
 	MTBfolder = fileparts(fileparts(which(mfilename)));
 	options.evalCode = false;
   options.format = 'html';
   options.outputDir = 'docs';
 	cd(MTBfolder);
-	d = rdir(fullfile('**', filesep, '**.m'));
+	d = rdir(fullfile(dir, filesep, '**.m'));
 	for i = 1:length(d)
     publish(d(i).name, options);
   end
