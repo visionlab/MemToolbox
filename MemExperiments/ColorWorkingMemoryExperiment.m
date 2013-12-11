04ea76549069865a7016c7b7ee236e27d344936f
Jordan Suchow <suchow@post.harvard.edu>
2013-12-11 01:58:12 -0500

    Improve style of experiment code

--- MemExperiments/ColorWorkingMemoryExperiment.m
+++ MemExperiments/ColorWorkingMemoryExperiment.m
@@ -15,47 +15,49 @@ function ColorWorkingMemoryExperiment()
     window = openWindow();
     prefs = getPreferences()
 
-    % put up instructions and wait for keypress
+    % Put up instructions and wait for keypress.
     instruct(window);
     returnToFixation(window, window.centerX, window.centerY, prefs.fixationSize)
 
     WaitSecs(1);
 
-    % get rects for items
+    % Get rects for each item.
+    rects = cell(1, max(prefs.setSizes));
     for i = 1:max(prefs.setSizes)
-      rects{i} = circularArrayRects([0, 0, prefs.squareSize, prefs.squareSize], i, prefs.radius, window.centerX, window.centerY)';
+      rects{i} = circularArrayRects([0, 0, prefs.squareSize, prefs.squareSize], ...
+        i, prefs.radius, window.centerX, window.centerY)';
     end
 
     colorWheelLocations = colorwheelLocations(window,prefs);
 
     for trialIndex = 1:length(prefs.fullFactorialDesign)
 
-      % figure out how many items on this trial and the duration
+      % Determine how many items there are on this trial and the duration.
       nItems = prefs.setSizes(prefs.fullFactorialDesign(prefs.order(trialIndex), 1));
       retentionInterval = prefs.retentionIntervals(prefs.fullFactorialDesign(prefs.order(trialIndex), 2));
 
-      % pick item to test
+      % Pick an item to test.
       itemToTest(trialIndex) = RandSample(1:nItems);
 
-      % pick colors for this trial
-      colorsInDegrees{trialIndex} = ceil(rand(1,nItems)*360);
+      % Pick the colors for this trial.
+      colorsInDegrees{trialIndex} = ceil(rand(1, nItems)*360);
 
-      % draw fixation
+      % Draw fixation.
       drawFixation(window, window.centerX, window.centerY, prefs.fixationSize);
 
-      % draw stimulus
+      % Draw the stimulus.
       colorsToDisplay = prefs.colorwheel(colorsInDegrees{trialIndex}, :)';
       Screen('FillRect', window.onScreen, colorsToDisplay, rects{nItems});
 
-      % post stimulus and wait
+      % Post the stimulus and wait.
       Screen('Flip', window.onScreen);
       WaitSecs(prefs.stimulusDuration);
 
-      % remove stimulus, return to blank, wait for retention interval to pass
+      % Remove stimulus, return to blank, wait for retention interval to pass.
       returnToFixation(window, window.centerX, window.centerY, prefs.fixationSize);
       WaitSecs(retentionInterval);
 
-      % choose a circle to test, then display response screen
+      % Choose a circle to test, then display the response screen.
       data.presentedColor(trialIndex) = deg2rad(colorsInDegrees{trialIndex}(itemToTest(trialIndex)));
       colorsOfTest = repmat([120 120 120], nItems, 1);
       colorsOfTest(itemToTest(trialIndex), :) = [145 145 145];
@@ -64,15 +66,14 @@ function ColorWorkingMemoryExperiment()
 
       drawColorWheel(window, prefs);
 
-      % wait for click
-
+      % Wait for click.
       SetMouse(window.centerX, window.centerY);
       ShowCursor('Arrow');
 
-      % if mouse button is already down, wait for release
-      [x,y,buttons] = GetMouse(window.onScreen);
+      % If mouse button is already down, wait for release.
+      GetMouse(window.onScreen);
       while any(buttons)
-        [x,y,buttons] = GetMouse(window.onScreen);
+        [x, y, buttons] = GetMouse(window.onScreen);
       end
 
       everMovedFromCenter = false;
@@ -105,14 +106,13 @@ function ColorWorkingMemoryExperiment()
 
       HideCursor
 
-      % return to fixation
+      % Return to fixation.
       returnToFixation(window, window.centerX, window.centerY, prefs.fixationSize);
       WaitSecs(0.5);
     end
 
-    % initial analysis of results
+    % Preliminary analysis of results.
     data.error = (180/pi) .* (angle(exp(1i*data.reportedColor)./exp(1i*data.presentedColor)));
-
     data.setSize = prefs.setSizes(prefs.fullFactorialDesign(prefs.order, 1));
     data.retentionInterval = prefs.retentionIntervals(prefs.fullFactorialDesign(prefs.order,2));
 
@@ -131,12 +131,12 @@ function prepareEnvironment
   clear all;
   HideCursor;
 
-  commandwindow; % select the command window to avoid typing in open scripts
+  commandwindow; % Select the command window to avoid typing in open scripts
 
-  % seed the random number generator
-  RandStream.setDefaultStream(RandStream('mt19937ar','seed',sum(100*clock)));
+  % Seed the random number generator.
+  RandStream.setDefaultStream(RandStream('mt19937ar', 'seed', sum(100*clock)));
 
-  ListenChar(2); % don't print to MATLAB command window
+  ListenChar(2); % Don't print to MATLAB command window
 end
 
 function postpareEnvironment
@@ -158,7 +158,8 @@ end
 
 function offsets = circularArrayOffsets(n, centerX, centerY, radius, rotation)
   degreeStep = 360/n;
-  offsets = [sind([0:degreeStep:(360-degreeStep)] + rotation)'.* radius, cosd([0:degreeStep:(360-degreeStep)] + rotation)'.* radius];
+  offsets = [sind(0:degreeStep:(360-degreeStep) + rotation)'.* radius, ...
+             cosd(0:degreeStep:(360-degreeStep) + rotation)'.* radius];
 end
 
 function rects = circularArrayRects(rect, nItems, radius, centerX, centerY)
@@ -174,16 +175,16 @@ end
 
 function window = openWindow()
   window.screenNumber = max(Screen('Screens'));
-  [window.onScreen rect] = Screen('OpenWindow', window.screenNumber, [128 128 128],[],[],[],[]);
+  window.onScreen = Screen('OpenWindow', window.screenNumber, [128 128 128]);
   Screen('BlendFunction', window.onScreen, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
   [window.screenX, window.screenY] = Screen('WindowSize', window.onScreen); % check resolution
-  window.screenRect  = [0 0 window.screenX window.screenY]; % screen rect
+  window.screenRect  = [0, 0, window.screenX, window.screenY]; % screen rect
   window.centerX = window.screenX * 0.5; % center of screen in X direction
   window.centerY = window.screenY * 0.5; % center of screen in Y direction
-  window.centerXL = floor(mean([0 window.centerX])); % center of left half of screen in X direction
-  window.centerXR = floor(mean([window.centerX window.screenX])); % center of right half of screen in X direction
+  window.centerXL = floor(mean([0, window.centerX])); % center of left half of screen in X direction
+  window.centerXR = floor(mean([window.centerX, window.screenX])); % center of right half of screen in X direction
 
-  % basic drawing and screen variables
+  % Basic drawing and screen variables.
   window.black    = BlackIndex(window.onScreen);
   window.white    = WhiteIndex(window.onScreen);
   window.gray     = mean([window.black window.white]);
@@ -192,15 +193,15 @@ function window = openWindow()
 end
 
 function drawColorWheel(window, prefs)
-  colorWheelLocations = [cosd([1:360]).*prefs.colorWheelRadius + window.centerX; ...
-    sind([1:360]).*prefs.colorWheelRadius + window.centerY];
+  colorWheelLocations = [cosd(1:360).*prefs.colorWheelRadius + window.centerX; ...
+    sind(1:360).*prefs.colorWheelRadius + window.centerY];
   colorWheelSizes = 20;
   Screen('DrawDots', window.onScreen, colorWheelLocations, colorWheelSizes, prefs.colorwheel', [], 1);
 end
 
 function L = colorwheelLocations(window,prefs)
-  L = [cosd([1:360]).*prefs.colorWheelRadius + window.centerX; ...
-    sind([1:360]).*prefs.colorWheelRadius + window.centerY];
+  L = [cosd(1:360).*prefs.colorWheelRadius + window.centerX; ...
+       sind(1:360).*prefs.colorWheelRadius + window.centerY];
 end
 
 function prefs = getPreferences
@@ -212,12 +213,12 @@ function prefs = getPreferences
   prefs.radius = 180;
   prefs.fixationSize = 3;
 
-  % colorwheel details
+  % Colorwheel details.
   prefs.colorWheelRadius = 350;
   prefs.colorwheel = load('colorwheel360.mat', 'fullcolormatrix');
   prefs.colorwheel = prefs.colorwheel.fullcolormatrix;
 
-  % randomize trial order of full factorial design order
+  % Randomize trial order of full factorial design order.
   prefs.fullFactorialDesign = fullfact([length(prefs.setSizes), ...
     length(prefs.retentionIntervals), ...
     prefs.nTrialsPerCondition]);
